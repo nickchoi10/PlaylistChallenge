2e888ed5a38fe64d180dfc787e5a3e80d243d607
Nick Choi <96934316+nickchoi10@users.noreply.github.com>
2022-07-26 23:14:51 -0400

    completed lab

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,32 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
+
+
+        int left=0;
+        int right=0;
+        int l=startIndex;
+        int r=startIndex;
+
+        for(int i =0; i< playList.length; i++){
+            if(this.playList[l]==selection){
+                return left;
+            }else if(this.playList[r]==selection){
+                return right;
+            }
+            l--;
+            r++;
+            if (l<0){
+                l = playList.length-1;
+            }
+            if(r> playList.length-1);{
+                r=0;
+            }
+            right++;
+            left++;
+        }
+
+
         return null;
     }
 }
